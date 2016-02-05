2653200453c1a708b2e5f5e33614f6e6b6c13431
fanzhengchen <fanzhengchen@gmail.com>
2016-02-06 02:16:39 +0800

    delete eclipse

--- javaInstaller.sh
+++ javaInstaller.sh
@@ -1,5 +1,3 @@
-sudo apt-get install eclipse -y
-sudo apt-get install eclipse-cdt -y
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update -y
 sudo apt-get install oracle-java8-installer -y
